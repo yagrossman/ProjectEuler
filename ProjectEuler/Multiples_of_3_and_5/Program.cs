a2a9cb7de43fd9889ed514d9f727e3f7069807fc
Yehuda Grossman <yagrossman@outlook.com>
2016-02-17 16:08:27 -0500

    fix for double test

--- ProjectEuler/Multiples_of_3_and_5/Program.cs
+++ ProjectEuler/Multiples_of_3_and_5/Program.cs
@@ -16,23 +16,34 @@ namespace Multiples_of_3_and_5
          int[] validMultiples = new int[maxValue];
          for (int i = 1; i < maxValue; i++)
          {
-            if ((3 * i) < maxValue)//valid multiple of 3
+            int mult3 = 3 * i;
+            int mult5 = 5 * i;
+            if(mult3 == mult5 && mult3 < maxValue)
             {
-               validMultiples[count] = 3 * i;
+               validMultiples[count] = mult3;
+               count++;
+               break;
+            }
+            if (mult3 < maxValue)//valid multiple of 3
+            {
+               validMultiples[count] = mult3;
                count++;
             }
-            if ((5 * i) < maxValue)//valid multiple of 5
+            if (mult5 < maxValue)//valid multiple of 5
             {
-               validMultiples[count] = 5 * i;
+               validMultiples[count] = mult5;
                count++;
             }
          }
-         //remove doubles
-         int x = 0;
-         for (int y = 1; y < validMultiples.Length; y++)
+         /*//remove doubles
+         for (int x = 0; x < validMultiples.Length; x++)
          {
-            if (validMultiples[x] == validMultiples[y]) validMultiples[y] = 0;
-         }
+            for (int y = 0; y < validMultiples.Length; y++)
+            {
+               if (validMultiples[x] == validMultiples[y]) validMultiples[y] = 0;
+            }
+         }*/
+
          int sumValidMultiples = 0;
          for (int z = 0; z < validMultiples.Length; z++)//sum up the valid multiples
          {
