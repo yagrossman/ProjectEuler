38261c6fbe38276e22e3114932c349654367d637
Yehuda Grossman <yagrossman@outlook.com>
2016-02-17 15:48:26 -0500

    Fix output 2

--- ProjectEuler/Multiples_of_3_and_5/Program.cs
+++ ProjectEuler/Multiples_of_3_and_5/Program.cs
@@ -34,6 +34,7 @@ namespace Multiples_of_3_and_5
          }
          Console.WriteLine("The sum of all the multiples of 3 or 5 below "+ maxValue+":");
          Console.WriteLine(sumValidMultiples);
+         Console.WriteLine("Press Enter to Exit.");
          Console.ReadLine();
       }
    }
