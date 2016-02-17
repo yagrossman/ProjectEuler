dc3b8ed25996eb028efa8f68883639f4812d345b
Yehuda Grossman <yagrossman@outlook.com>
2016-02-17 15:42:31 -0500

    create logic

--- ProjectEuler/Multiples_of_3_and_5/Program.cs
+++ ProjectEuler/Multiples_of_3_and_5/Program.cs
@@ -10,7 +10,30 @@ namespace Multiples_of_3_and_5
    {
       static void Main(string[] args)
       {
-
+         int count = 0;
+         int[] validMultiples = { 0 };
+         Console.WriteLine("Please Enter Max Value: ");
+         int maxValue = int.Parse(Console.ReadLine());
+         for (int i = 1; i < maxValue; i++)
+         {
+            if ((3 * i) < maxValue)//valid multiple of 3
+            {
+               validMultiples[count] = 3 * i;
+               count++;
+            }
+            else if ((5 * i) < maxValue)//valid multiple of 5
+            {
+               validMultiples[count] = 5 * i;
+               count++;
+            }
+         }
+         int sumValidMultiples = 0;
+         for (int x = 0; x < validMultiples.Length; x++)//sum up the valid multiples
+         {
+            sumValidMultiples += validMultiples[x];
+         }
+         Console.WriteLine(sumValidMultiples);
+         Console.ReadLine();
       }
    }
 }
