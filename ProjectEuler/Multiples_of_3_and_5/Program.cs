a6939eed71433199c53ce80f4b852c1d2017bf6a
Yehuda Grossman <yagrossman@outlook.com>
2016-03-08 20:30:26 -0500

    rewrote code for Multiples_of_3_and_5 to be simpler

--- ProjectEuler/Multiples_of_3_and_5/Program.cs
+++ ProjectEuler/Multiples_of_3_and_5/Program.cs
@@ -10,35 +10,16 @@ namespace Multiples_of_3_and_5
    {
       static void Main(string[] args)
       {
-         int count = 0;
+         int sumMultiples = 0;
          Console.WriteLine("Please Enter Max Value: ");
          int maxValue = int.Parse(Console.ReadLine());
-         int[] validMultiples = new int[maxValue];
          for (int i = 1; i < maxValue; i++)
          {
-            int mult3 = 3 * i;
-            int mult5 = 5 * i;
-            if (mult3 < maxValue)//valid multiple of 3
-            {
-               validMultiples[count] = mult3;
-               count++;
-            }
-            if (mult5 < maxValue)//valid multiple of 5
-            {
-               validMultiples[count] = mult5;
-               count++;
-            }
+            if (i % 3 == 0 || i % 5 == 0)
+               sumMultiples += i;
          }
-         //remove doubles
-         var uniqueValidMultiples = validMultiples.Distinct().ToArray();
-         //sum em up!!!
-         int sumValidMultiples = 0;
-         for (int z = 0; z < uniqueValidMultiples.Length; z++)//sum up the valid multiples
-         {
-            sumValidMultiples += uniqueValidMultiples[z];
-         }
-         Console.WriteLine("The sum of all the multiples of 3 or 5 below "+ maxValue+":");
-         Console.WriteLine(sumValidMultiples);
+         Console.WriteLine("The sum of all the multiples of 3 or 5 below "+ maxValue+" is:");
+         Console.WriteLine(sumMultiples);
          Console.WriteLine("Press Enter to Exit.");
          Console.ReadLine();
       }
