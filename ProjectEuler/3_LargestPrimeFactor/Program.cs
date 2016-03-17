45b215c3b2df0c5af7edd0c949c0c5c1f6f43aa5
Yehuda Grossman <yagrossman@outlook.com>
2016-03-17 15:44:33 -0400

    completed project euler question 3

---
+++ ProjectEuler/3_LargestPrimeFactor/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_LargestPrimeFactor
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         double number = 600851475143;
+         Console.WriteLine(largestPrimeFactor(number));
+         Console.Read();
+      }
+
+      public static double largestPrimeFactor(double number)
+      {
+         double[] factors = new double[1000];
+         int factorsPos = 0;
+         int d = 2;
+         while (number > 1)
+         {
+            while (number % d == 0)
+            {
+               factors[factorsPos++] = d;
+               number /= d;
+            }
+            d = d + 1;
+            if (d * d > number)
+            {
+               if (number > 1)
+               {
+                  factors[factorsPos++] = number;
+                  break;
+               }
+            }
+         }
+         double largestPrime = factors.Max();
+         return largestPrime;
+      }
+   }
+}
